724de446ae7e3738032df3f332d71ae37e2bcb8b
forifelse <forifelse@163.coom>
2017-06-13 15:10:50 +0800

    add fisp.h

---
+++ Engine/include/fisp.h
@@ -0,0 +1,30 @@
+/*-----------------------------------------------------------
+Name:		fisp
+Author:		fisper@163.com
+Date:		2016.12
+Copyright (c). All rights reserved.
+------------------------------------------------------------*/
+
+#pragma once
+#include "world.h"
+
+namespace fisp
+{
+	namespace engine
+	{
+		/*-----------------------------------------------------------
+		class Fisp
+		------------------------------------------------------------*/
+		class Fisp : public IRoot
+		{
+		public:
+			Fisp();
+			virtual ~Fisp();
+
+		private:
+			
+		};
+
+
+	}
+}
